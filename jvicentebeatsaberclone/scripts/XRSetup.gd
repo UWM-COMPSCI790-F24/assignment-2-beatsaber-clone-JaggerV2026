2cb0579723936bc1b26b15ad946c19258ba79a90
JaggerV2026 <123402353+JaggerV2026@users.noreply.github.com>
2024-10-09 21:41:58 -0500

    Added XRSetup script
    Created XRSetup script and attached it to XROrigin3D.

---
+++ jvicentebeatsaberclone/scripts/XRSetup.gd
@@ -0,0 +1,12 @@
+extends XROrigin3D
+
+var xr_interface : XRInterface
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		get_viewport().use_xr = true
+	else:
+		print("OpenXR not initialized. Please check if your headset is connected")
